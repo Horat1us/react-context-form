1b8daf169389343ed33ac9bef9eae120e77018f0
Alexander Letnikow <reclamme@gmail.com>
2017-08-31 15:36:14 +0300

    Fix FormGroupContext proptypes import

--- src/FormGroup/FormGroupContext.ts
+++ src/FormGroup/FormGroupContext.ts
@@ -1,3 +1,5 @@
+import * as PropTypes from "prop-types"
+
 import {LabelContext, LabelContextTypes} from "../Label";
 import {HintContext, HintContextTypes} from "../Hint";
 import {InputContext, InputContextTypes} from "../Input/InputContext";
