9755bfd70a19779513ef04fc00bfc88e18eea17c
Dmitry <makarenko.dmitry@wearesho.com>
2017-12-11 13:07:22 +0400

    Date.now() => perfomance.now()

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -1,4 +1,10 @@
 import * as chai from 'chai';
 import * as chaiEnzyme from 'chai-enzyme';
 
+if ("performance" in window === false) {
+    (window as any).performance = {
+        now: () => Date.now(),
+    };
+}
+
 chai.use(chaiEnzyme());
